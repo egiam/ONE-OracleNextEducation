f4504b3b48c9294ceccdf16f9c5c68441d9c4a66
Ezequiel Giampaoli <ezegiampaoli@gmail.com>
2023-04-09 17:09:14 -0700

    Update

--- 9 SQL con MySQL Server de Oracle/1-consultas-sql-avanzando-en-sql-con-my-sql-proyecto-final/comandos.sql
+++ 9 SQL con MySQL Server de Oracle/1-consultas-sql-avanzando-en-sql-con-my-sql-proyecto-final/comandos.sql
@@ -90,7 +90,8 @@ SELECT DISTINCT ENVASE, TAMANO FROM tabla_de_productos;
 SELECT DISTINCT ENVASE, TAMANO, SABOR FROM tabla_de_productos
 WHERE SABOR = 'Naranja';
 
-/* CONSULTAS LIMIT */
+/* CONSULTAS LIMIT
+Limita el numero de registros que exhibe */
 
 SELECT * FROM tabla_de_productos;
 
@@ -98,7 +99,14 @@ SELECT * FROM tabla_de_productos LIMIT 5;
 
 SELECT * FROM tabla_de_productos LIMIT 5,4;
 
-/* CONSULTAS ORDER BY */
+
+-- Queremos obtener las 10 primeras ventas del día 01/01/2017. ¿Cuál sería el comando SQL para obtener este resultado?
+
+SELECT * FROM facturas  WHERE FECHA_VENTA = '2017-01-01' LIMIT 10;
+
+
+/* CONSULTAS ORDER BY
+Ordena en base a los campos los registros exhibidos */
 
 SELECT * FROM tabla_de_productos;
 
@@ -112,7 +120,20 @@ SELECT * FROM tabla_de_productos ORDER BY NOMBRE_DEL_PRODUCTO DESC;
 
 SELECT * FROM tabla_de_productos ORDER BY ENVASE DESC, NOMBRE_DEL_PRODUCTO ASC;
 
-/* CONSULTAS GROUP BY */
+-- ¿Cuál (o cuáles) fue (fueron) la(s) mayor(es) venta(s) del producto “Refrescante, 1 Litro, Frutilla/Limón”, en cantidad? 
+-- (Obtenga este resultado utilizando 2 comandos SQL).
+
+SELECT CODIGO_DEL_PRODUCTO FROM tabla_de_productos 
+WHERE NOMBRE_DEL_PRODUCTO = "Refrescante" AND TAMANO = "1 Litro" 
+AND SABOR = "Frutilla/Limón"; 
+
+-- Ahora, con el código del producto que es: 1101035, podemos consultar la cantidad:
+
+SELECT * FROM items_facturas WHERE CODIGO_DEL_PRODUCTO = "1101035" ORDER BY CANTIDAD DESC;
+
+
+/* CONSULTAS GROUP BY
+Groupa los registros por cada campo */
 
 SELECT ESTADO, LIMITE_DE_CREDITO FROM tabla_de_clientes;
 
@@ -147,7 +168,16 @@ WHERE EDAD >=20
 GROUP BY ESTADO, BARRIO
 ORDER BY EDAD;
 
-/* CONSULTAS HAVING */
+-- Aprovechando el ejercicio del video anterior ¿Cuántos ítems vendidos cuentan con la mayor cantidad del producto '1101035'?
+
+SELECT MAX(CANTIDAD) AS CANTIDAD_MAXIMA FROM items_facturas WHERE CODIGO_DEL_PRODUCTO = "1101035";
+
+-- Notaremos que la cantidad máxima vendida es de 99. Así, podemos entonces ejecutar el siguiente comando:
+
+SELECT COUNT(*) FROM items_facturas WHERE CODIGO_DEL_PRODUCTO = "1101035" AND CANTIDAD = 99;
+
+/* CONSULTAS HAVING
+Having es parecido a Where, nada mas que se utiliza con las 'funciones' (ej: sum, max, avg, ...) */
 
 SELECT ESTADO, SUM(LIMITE_DE_CREDITO) AS LIMITE_TOTAL
 FROM tabla_de_clientes GROUP BY ESTADO;
@@ -183,7 +213,15 @@ FROM tabla_de_productos GROUP BY ENVASE
 HAVING SUM(PRECIO_DE_LISTA) >= 80 
 AND MAX(PRECIO_DE_LISTA) >= 5;
 
-/* CONSULTAS CASE */
+-- ¿Quiénes fueron los clientes que realizaron más de 2000 compras en 2016?
+
+SELECT DNI, COUNT(*) FROM facturas
+WHERE YEAR(FECHA_VENTA) = 2016
+GROUP BY DNI
+HAVING COUNT(*) > 2000;
+
+/* CONSULTAS CASE
+Realiza un test a uno o mas campos y dependiendo del resultado obtendremos un valor especifico */
 
 SELECT * FROM tabla_de_productos;
 
@@ -211,10 +249,27 @@ CASE
 END
 ORDER BY ENVASE;
 
+
+/*
+Registre el año de nacimiento de los clientes y clasifíquelos de la siguiente manera:
+
+Nacidos antes de 1990= Viejos, nacidos entre 1990 y 1995= Jóvenes y nacidos después de 1995= Niños. Liste el nombre del cliente y esta clasificación.
+*/
+
+SELECT NOMBRE,
+CASE 
+    WHEN YEAR(fecha_de_nacimiento) < 1990 THEN 'Viejos'
+    WHEN YEAR(fecha_de_nacimiento) >= 1990 
+    AND YEAR(fecha_de_nacimiento) <= 1995 THEN 'Jóvenes' 
+    ELSE 'Niños' 
+END AS CLASIFICACION_EDAD
+FROM tabla_de_clientes;
+
 /* PROYECTO DEL AULA ANTERIOR 4 
 Los comandos vistos en el aula anterior fueron los siguientes:*/
 
-/* CONSULTAS INNER JOIN */
+/* CONSULTAS INNER JOIN
+Nos permite unir dos o mas tablas a traves de sus 'keys' */
 
 SELECT * FROM tabla_de_vendedores;
 
@@ -235,6 +290,16 @@ SELECT A.NOMBRE, B.MATRICULA, COUNT(*)
 FROM tabla_de_vendedores A, facturas B
 WHERE A.MATRICULA = B.MATRICULA
 GROUP BY A.NOMBRE, B.MATRICULA;
+
+-- Obtén la facturación anual de la empresa. 
+-- Ten en cuenta que el valor financiero de las ventas consiste en multiplicar la cantidad por el precio.
+SELECT YEAR(FECHA_VENTA), SUM(CANTIDAD * PRECIO) AS FACTURACION
+FROM facturas F 
+INNER JOIN 
+items_facturas IFa 
+ON F.NUMERO = IFa.NUMERO
+GROUP BY YEAR(FECHA_VENTA);
+
 
 /* CONSULTAS LEFT Y RIGHT JOIN */
 
@@ -302,7 +367,8 @@ SELECT tabla_de_clientes.NOMBRE, tabla_de_clientes.CIUDAD, tabla_de_clientes.BAR
 tabla_de_vendedores.NOMBRE, VACACIONES
 FROM tabla_de_clientes, tabla_de_vendedores;
 
-/* CONSULTAS UNION Y FULL JOIN */
+/* CONSULTAS UNION Y FULL JOIN
+Union permite unir 2 o mas tablas, pero el numero de campos, el tipo de campo, etc deben ser iguales para poder completar la union */
 
 SELECT DISTINCT BARRIO FROM tabla_de_clientes;
 
@@ -342,13 +408,15 @@ RIGHT JOIN
 tabla_de_vendedores
 ON tabla_de_clientes.BARRIO = tabla_de_vendedores.BARRIO;
 
-/* SUBCONSULTAS */
+/* SUBCONSULTAS
+Permite realizar una consulta en el interior de otra consulta */
 
 SELECT DISTINCT BARRIO FROM tabla_de_vendedores;
 
 SELECT * FROM tabla_de_clientes
 WHERE BARRIO IN ('Condesa','Del Valle','Contadero','Oblatos');
 
+-- Seleccionar todos los clientes donde el barrio sea el mismo de los vendedores
 SELECT * FROM tabla_de_clientes
 WHERE BARRIO IN (SELECT DISTINCT BARRIO FROM tabla_de_vendedores);
 
@@ -360,8 +428,18 @@ SELECT X.ENVASE, X.PRECIO_MAXIMO FROM
 AS PRECIO_MAXIMO FROM tabla_de_productos GROUP BY ENVASE) X
 WHERE X.PRECIO_MAXIMO >=10;
 
-/* VIEWS */
+SELECT X.DNI, X.CONTADOR FROM 
+(SELECT DNI, COUNT(*) AS CONTADOR FROM facturas
+WHERE YEAR(FECHA_VENTA) = 2016
+GROUP BY DNI) X WHERE X.CONTADOR > 2000;
 
+/* VIEWS
+Es una tabla logica que resulta de una consulta que puede ser utilizada posteriormente en cualquier otra consulta */
+
+SELECT ENVASE, MAX(PRECIO_DE_LISTA) 
+AS PRECIO_MAXIMO FROM tabla_de_productos GROUP BY ENVASE;
+
+CREATE OR REPLACE VIEW 'vw_envases_grandes' as 
 SELECT ENVASE, MAX(PRECIO_DE_LISTA) 
 AS PRECIO_MAXIMO FROM tabla_de_productos GROUP BY ENVASE;
 
@@ -387,38 +465,56 @@ Los comandos vistos en el aula anterior fueron los siguientes:*/
 
 /* FUNCIONES STRING */
 
+-- Elimina los espacios a la izquierda
 SELECT LTRIM("    MySQL es fácil");
 
+-- Elimina los espacios a la derecha
 SELECT RTRIM("MySQL es fácil    ");
 
+-- Elimina los espacios a la izq y a la der
 SELECT TRIM("    MySQL es fácil    ");
 
+-- Concatena uno o mas Strings
 SELECT CONCAT("MySQL es fácil,", " no lo crees?");
 
+-- Pasa a mayusculas todo el string
 SELECT UPPER("mysql es una base de datos interesante.");
 
+-- Pasa a minusculas todo el string
 SELECT LOWER("MYSQL ES UNA BASE DE DATOS INTERESANTE.");
 
+-- Selecciona una parte del string, dependiendo del posicionamiento y longitud que des
 SELECT SUBSTRING("mysql es una base de datos interesante.", 14,4);
 
 SELECT CONCAT(NOMBRE, " ", DNI) FROM tabla_de_clientes;
 
+-- Haz una consulta listando el nombre del cliente y la dirección completa (Con calle, barrio, ciudad y estado).
+SELECT NOMBRE, CONCAT(DIRECCION_1, ' ', BARRIO, ' ', CIUDAD, ' ', ESTADO) AS COMPLETO FROM tabla_de_clientes;
+
 /* FUNCIONES DATE */
 
+-- Selecciona la fecha actual
 SELECT CURDATE();
 
+-- Selecciona el dia y la hora actual
 SELECT current_timestamp();
 
+-- Selecciona solo el año actual
 SELECT YEAR(current_timestamp());
 
+-- Selecciona solo el mes actual
 SELECT MONTH(current_timestamp());
 
+-- Selecciona solo el dia actual
 SELECT DAY(current_timestamp());
 
+-- Selecciona el nombre del mes actual
 SELECT MONTHNAME(current_timestamp());
 
+-- Selecciona el nombre del dia actual
 SELECT DAYNAME(current_timestamp());
 
+-- Genera un resultado en int que da la diferencia entre cierta fecha y cierta otra fecha.
 SELECT DATEDIFF(current_timestamp(), "2021-01-01") AS DIFERENCIA_DE_DIAS;
 
 SELECT DATEDIFF(current_timestamp(), "1984-06-20") AS DIFERENCIA_DE_DIAS;
@@ -430,16 +526,24 @@ SELECT DISTINCT FECHA_VENTA,
 DAYNAME(FECHA_VENTA) AS DIA, MONTHNAME(FECHA_VENTA) AS MES, 
 YEAR(FECHA_VENTA) AS AÑO FROM facturas;
 
+-- Haz una consulta que muestre el nombre y la edad actual del cliente.
+SELECT NOMBRE, TIMESTAMPDIFF(YEAR, FECHA_DE_NACIMIENTO, CURDATE()) AS    EDAD
+FROM  tabla_de_clientes;
+
 /* FUNCIONES MATEMÁTICAS */
 
 SELECT (34+346-67)/15 * 29 AS RESULTADO;
 
+-- Redondea para arriba siempre
 SELECT CEILING (23.1222);
 
+-- Redondea para abajo siempre
 SELECT FLOOR (23.999999);
 
+-- Da un numero aleatoreo
 SELECT RAND() AS RESULTADO;
 
+-- Redondea en base a la cantidad de decimales que se le pide
 SELECT ROUND(254.545,2);
 
 SELECT ROUND(254.545,1);
@@ -449,6 +553,15 @@ CANTIDAD * PRECIO AS FACTURACIÓN FROM items_facturas;
 
 SELECT NUMERO, CANTIDAD, PRECIO, 
 ROUND(CANTIDAD * PRECIO,2) AS FACTURACIÓN FROM items_facturas;
+
+-- En la tabla de facturas tenemos el valor del impuesto. En la tabla de ítems tenemos la cantidad y la facturación. 
+-- Calcula el valor del impuesto pago en el año de 2016 redondeando al menor entero.
+SELECT YEAR(FECHA_VENTA), FLOOR(SUM(IMPUESTO * (CANTIDAD * PRECIO))) 
+AS RESULTADO
+FROM facturas F
+INNER JOIN items_facturas IFa ON F.NUMERO = IFa.NUMERO
+WHERE YEAR(FECHA_VENTA) = 2016
+GROUP BY YEAR(FECHA_VENTA);
 
 /* FUNCIONES CONVERSIÓN */
 
@@ -462,6 +575,23 @@ DATE_FORMAT(CURRENT_TIMESTAMP(),"%W, %d/%m/%Y a las %T" )) AS RESULTADO;
 SELECT CONVERT(23.45, CHAR) AS RESULTADO;
 
 SELECT SUBSTRING(CONVERT(23.45, CHAR),3,1) AS RESULTADO;
+
+
+
+-- Queremos construir un SQL cuyo resultado sea, para cada cliente:
+
+-- “El cliente Pepito Pérez facturó 120000 en el año 2016”.
+
+-- Solamente para el año 2016.
+
+SELECT CONCAT('El cliente ', TC.NOMBRE, ' facturó ', 
+CONVERT(SUM(IFa.CANTIDAD * IFa.precio), CHAR(20))
+ , ' en el año ', CONVERT(YEAR(F.FECHA_VENTA), CHAR(20))) AS FRASE FROM facturas F
+INNER JOIN items_facturas IFa ON F.NUMERO = IFa.NUMERO
+INNER JOIN tabla_de_clientes TC ON F.DNI = TC.DNI
+WHERE YEAR(FECHA_VENTA) = 2016
+GROUP BY TC.NOMBRE, YEAR(FECHA_VENTA);
+
 
 /* ****PROYECTO FINAL**** */
 
@@ -489,6 +619,44 @@ items_facturas IFa
 ON F.NUMERO = IFa.NUMERO
 GROUP BY
 F.DNI, DATE_FORMAT(F.FECHA_VENTA, "%m - %Y");
+
+
+
+
+-- En esta aula construimos un informe que presentó a los clientes que tenían ventas inválidas. Complementa este informe listando solamente a los que tuvieron ventas inválidas y calcula la diferencia entre el límite de venta máximo y la cantidad vendida en porcentuales.
+
+-- Tips:
+
+--     Utiliza el comando SQL empleado al final del video.
+
+--     Filtra solamente las líneas donde: (A.CANTIDAD_VENDIDA - A.CANTIDAD_MAXIMA) < 0
+
+--     Lista el campo X.CANTIDAD_LIMITE
+
+--     Crea nuevo campo ejecutando la fórmula: (1 - (X.QUANTIDADE_LIMITE/X.QUANTIDADE_VENDAS)) * 100.
+
+SELECT A.DNI, A.NOMBRE, A.MES_AÑO, 
+A.CANTIDAD_VENDIDA - A.CANTIDAD_MAXIMA AS DIFERENCIA,
+CASE
+   WHEN  (A.CANTIDAD_VENDIDA - A.CANTIDAD_MAXIMA) <= 0 THEN 'Venta Válida'
+   ELSE 'Venta Inválida'
+END AS STATUS_VENTA, ROUND((1 - (A.CANTIDAD_MAXIMA/A.CANTIDAD_VENDIDA)) * 100,2) AS PORCENTAJE
+ FROM(
+SELECT F.DNI, TC.NOMBRE, DATE_FORMAT(F.FECHA_VENTA, "%m - %Y") AS MES_AÑO, 
+SUM(IFa.CANTIDAD) AS CANTIDAD_VENDIDA, 
+MAX(VOLUMEN_DE_COMPRA)/10 AS CANTIDAD_MAXIMA  
+FROM facturas F 
+INNER JOIN 
+items_facturas IFa
+ON F.NUMERO = IFa.NUMERO
+INNER JOIN 
+tabla_de_clientes TC
+ON TC.DNI = F.DNI
+GROUP BY
+F.DNI, TC.NOMBRE, DATE_FORMAT(F.FECHA_VENTA, "%m - %Y"))A
+WHERE (A.CANTIDAD_MAXIMA - A.CANTIDAD_VENDIDA) < 0;
+
+
 
 /* LIMITE DE VENTAS POR CLIENTE (VOLUMEN EN DECILITROS)*/
 SELECT * FROM tabla_de_clientes TC;
@@ -543,6 +711,34 @@ ON TC.DNI = F.DNI
 GROUP BY
 F.DNI, TC.NOMBRE, DATE_FORMAT(F.FECHA_VENTA, "%m - %Y"))A;
 
+
+-- En esta aula construimos un informe que presentó a los clientes que tenían ventas inválidas. Ahora lista solamente a los que tuvieron ventas inválidas en el año 2018 excediendo más del 50% de su límite permitido por mes. Calcula la diferencia entre el límite de venta máximo y la cantidad vendida, en porcentuales.
+
+-- Tips:
+
+--     Te puedes apoyar en el código que realizaste para el desafío anterior.
+
+SELECT A.DNI, A.NOMBRE, A.MES_AÑO, 
+A.CANTIDAD_VENDIDA - A.CANTIDAD_MAXIMA AS DIFERENCIA,
+CASE
+   WHEN  (A.CANTIDAD_VENDIDA - A.CANTIDAD_MAXIMA) <= 0 THEN 'Venta Válida'
+   ELSE 'Venta Inválida'
+END AS STATUS_VENTA, ROUND((1 - (A.CANTIDAD_MAXIMA/A.CANTIDAD_VENDIDA)) * 100,2) AS PORCENTAJE
+ FROM(
+SELECT F.DNI, TC.NOMBRE, DATE_FORMAT(F.FECHA_VENTA, "%m - %Y") AS MES_AÑO, 
+SUM(IFa.CANTIDAD) AS CANTIDAD_VENDIDA, 
+MAX(VOLUMEN_DE_COMPRA)/10 AS CANTIDAD_MAXIMA  
+FROM facturas F 
+INNER JOIN 
+items_facturas IFa
+ON F.NUMERO = IFa.NUMERO
+INNER JOIN 
+tabla_de_clientes TC
+ON TC.DNI = F.DNI
+GROUP BY
+F.DNI, TC.NOMBRE, DATE_FORMAT(F.FECHA_VENTA, "%m - %Y"))A
+WHERE (A.CANTIDAD_MAXIMA - A.CANTIDAD_VENDIDA) < 0 AND ROUND((1 - (A.CANTIDAD_MAXIMA/A.CANTIDAD_VENDIDA)) * 100,2) > 50
+AND A.MES_AÑO LIKE "%2018";
 
 
 /* INFORME ANUAL DE VENTAS POR SABOR*/
@@ -640,3 +836,43 @@ WHERE YEAR(F.FECHA_VENTA) = 2016
 GROUP BY YEAR(F.FECHA_VENTA)) VENTA_TOTAL
 ON VENTA_TOTAL.AÑO = VENTAS_SABOR.AÑO
 ORDER BY VENTAS_SABOR.CANTIDAD_TOTAL DESC;
+
+
+
+-- Modifica el informe pero ahora para ver el ranking de las ventas por tamaño.
+
+-- Tips:
+
+--     Puede parecer difícil pero este es el ejercicio más fácil de resolver.
+
+SELECT VENTAS_TAMANO.TAMANO, VENTAS_TAMANO.AÑO, VENTAS_TAMANO.CANTIDAD_TOTAL,
+ROUND((VENTAS_TAMANO.CANTIDAD_TOTAL/VENTA_TOTAL.CANTIDAD_TOTAL)*100,2) 
+AS PORCENTAJE FROM (
+SELECT P.TAMANO, SUM(IFa.CANTIDAD) AS CANTIDAD_TOTAL, 
+YEAR(F.FECHA_VENTA) AS AÑO FROM
+tabla_de_productos P
+INNER JOIN
+items_facturas IFa
+ON P.CODIGO_DEL_PRODUCTO = IFa.CODIGO_DEL_PRODUCTO
+INNER JOIN
+facturas F
+ON F.NUMERO = IFa.NUMERO
+WHERE YEAR(F.FECHA_VENTA) = 2016
+GROUP BY P.TAMANO, YEAR(F.FECHA_VENTA)
+ORDER BY SUM(IFa.CANTIDAD) DESC) VENTAS_TAMANO
+INNER JOIN (
+SELECT SUM(IFa.CANTIDAD) AS CANTIDAD_TOTAL, 
+YEAR(F.FECHA_VENTA) AS AÑO FROM
+tabla_de_productos P
+INNER JOIN
+items_facturas IFa
+ON P.CODIGO_DEL_PRODUCTO = IFa.CODIGO_DEL_PRODUCTO
+INNER JOIN
+facturas F
+ON F.NUMERO = IFa.NUMERO
+WHERE YEAR(F.FECHA_VENTA) = 2016
+GROUP BY YEAR(F.FECHA_VENTA)) VENTA_TOTAL
+ON VENTA_TOTAL.AÑO = VENTAS_TAMANO.AÑO
+ORDER BY VENTAS_TAMANO.CANTIDAD_TOTAL DESC;
+
+
